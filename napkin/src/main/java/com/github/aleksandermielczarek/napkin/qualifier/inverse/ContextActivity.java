ce8576e58645c7e38cdff1c561112a15e9939e46
Aleksander Mielczarek <alekmiel91@gmail.com>
2016-05-13 21:01:23 +0200

    Add new annotations

---
+++ napkin/src/main/java/com/github/aleksandermielczarek/napkin/qualifier/inverse/ContextActivity.java
@@ -0,0 +1,10 @@
+package com.github.aleksandermielczarek.napkin.qualifier.inverse;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Aleksander Mielczarek on 13.05.2016.
+ */
+@Qualifier
+public @interface ContextActivity {
+}
---
+++ napkin/src/main/java/com/github/aleksandermielczarek/napkin/scope/per/PerFragment.java
@@ -0,0 +1,14 @@
+package com.github.aleksandermielczarek.napkin.scope.per;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Aleksander Mielczarek on 13.05.2016.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerFragment {
+}
