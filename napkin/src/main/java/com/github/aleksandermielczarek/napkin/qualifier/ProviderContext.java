19a359bc4c4f8a710d9062dd7efa97993a31e542
Aleksander Mielczarek <aleksander.mielczarek@comarch.com>
2016-10-31 14:39:58 +0100

    Add new annotations

---
+++ napkin/src/main/java/com/github/aleksandermielczarek/napkin/qualifier/ProviderContext.java
@@ -0,0 +1,11 @@
+package com.github.aleksandermielczarek.napkin.qualifier;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Aleksander Mielczarek on 10/31/2016.
+ */
+
+@Qualifier
+public @interface ProviderContext {
+}
